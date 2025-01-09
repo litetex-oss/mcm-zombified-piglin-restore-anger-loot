7b0d75dc48a51a055a09fa85e811092ba192e0ee
litetex <40789489+litetex@users.noreply.github.com>
2025-01-09 12:09:39 +0100

    Create ZombifiedPiglinMixin.java

---
+++ src/main/java/net/litetex/zpral/mixin/ZombifiedPiglinMixin.java
@@ -0,0 +1,11 @@
+package net.litetex.zpral.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.entity.mob.ZombifiedPiglinEntity;
+
+
+@Mixin(ZombifiedPiglinEntity.class)
+public class ZombifiedPiglinMixin
+{
+}
