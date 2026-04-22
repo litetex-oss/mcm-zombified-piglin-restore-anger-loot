529aa36760579688ae963833d914515cdc1854a8
litetex <40789489+litetex@users.noreply.github.com>
2026-04-22 22:04:50 +0200

    Use correct blockGetter to not cause deadlock
    Fixes #170
    
    Currently the world generation / server thread deadlocks when a strider-zombified piglin chockey spawns as part of the world generation.
    This happens because accessing the block-state via `current.level()` happens from the world generation thread, tries to re-schedule on the main thread but the main thread is currently waiting for the world generation -> Deadlock

--- src/main/java/net/litetex/zpral/mixin/ZombifiedPiglinMixin.java
+++ src/main/java/net/litetex/zpral/mixin/ZombifiedPiglinMixin.java
@@ -1,6 +1,7 @@
 package net.litetex.zpral.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -15,12 +16,14 @@ import net.minecraft.world.entity.monster.zombie.ZombifiedPiglin;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.block.Blocks;
 
 
 @Mixin(ZombifiedPiglin.class)
 @SuppressWarnings("javabugs:S6320")
-public abstract class ZombifiedPiglinMixin
+public abstract class ZombifiedPiglinMixin extends ZombieMixin
 {
 	@Inject(
 		method = "customServerAiStep",
@@ -28,7 +31,7 @@ public abstract class ZombifiedPiglinMixin
 			value = "INVOKE",
 			target = "Lnet/minecraft/world/entity/monster/zombie/Zombie;customServerAiStep"
 				+ "(Lnet/minecraft/server/level/ServerLevel;)V"))
-	protected void mobTick(final ServerLevel world, final CallbackInfo ci)
+	protected void mobTick(final ServerLevel level, final CallbackInfo ci)
 	{
 		final ZombifiedPiglin current = (ZombifiedPiglin)(Object)this;
 		if(current.isAngry())
@@ -58,14 +61,47 @@ public abstract class ZombifiedPiglinMixin
 		cancellable = true
 	)
 	public void populateDefaultEquipmentSlots(
-		final RandomSource randomSource,
-		final DifficultyInstance difficultyInstance,
+		final RandomSource random,
+		final DifficultyInstance difficulty,
 		final CallbackInfo ci)
 	{
 		final ZombifiedPiglin current = (ZombifiedPiglin)(Object)this;
-		if(current.level() instanceof final ServerLevel serverLevel
-			&& serverLevel.getGameRules().get(ZPRALGameRules.SPAWN_ZOMBIFIED_PIGLIN_ALWAYS_WITH_SWORD_ON_MAGMA_BLOCK)
-			&& serverLevel.getBlockState(current.blockPosition().below()).is(Blocks.MAGMA_BLOCK))
+		
+		// This is the case when finalizeSpawn is called, e.g. when:
+		// A) spawning normally
+		// B) spawning during world generation (e.g. riding a strider)
+		//
+		// We need to get the correct ServerLevelAccessor here because when the world is generating (case B above)
+		// current.level() is not yet ready to getBlockStates which causes a deadlock; see #170 for details
+		final ServerLevelAccessor finalizeSpawnServerLevelAccessor = this.refFinalizeSpawnServerLevelAccessor.get();
+		if(finalizeSpawnServerLevelAccessor != null)
+		{
+			this.maybeSpawnWithGoldSwordOnlyOnMagmaBlock(
+				current,
+				finalizeSpawnServerLevelAccessor.getLevel(),
+				finalizeSpawnServerLevelAccessor,
+				ci);
+		}
+		// In Vanilla this can only happen if a pig is struck by lightning
+		else if(current.level() instanceof final ServerLevel currentServerLevel)
+		{
+			this.maybeSpawnWithGoldSwordOnlyOnMagmaBlock(
+				current,
+				currentServerLevel,
+				currentServerLevel,
+				ci);
+		}
+	}
+	
+	@Unique
+	void maybeSpawnWithGoldSwordOnlyOnMagmaBlock(
+		final ZombifiedPiglin current,
+		final ServerLevel serverLevel,
+		final BlockGetter blockGetter,
+		final CallbackInfo ci)
+	{
+		if(serverLevel.getGameRules().get(ZPRALGameRules.SPAWN_ZOMBIFIED_PIGLIN_ALWAYS_WITH_SWORD_ON_MAGMA_BLOCK)
+			&& blockGetter.getBlockState(current.blockPosition().below()).is(Blocks.MAGMA_BLOCK))
 		{
 			current.setItemSlot(EquipmentSlot.MAINHAND, new ItemStack(Items.GOLDEN_SWORD));
 			ci.cancel();
